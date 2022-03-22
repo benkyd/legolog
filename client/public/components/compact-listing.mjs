3b9c7fa801cb7cf5789487fd9feb083fe598e3dc
Ben <36240171+benkyd@users.noreply.github.com>
2022-03-22 18:31:46 +0000

    documentation and more components
    Former-commit-id: 6305ac7a9d93ebc8c4d6316068128603177556c4

---
+++ client/public/components/compact-listing.mjs
@@ -0,0 +1,22 @@
+import { RegisterComponent, Component, SideLoad } from './components.mjs';
+
+class CompactProductListing extends Component {
+    static __IDENTIFY => 'compact-listing';
+
+    constructor() {
+        super(CompactProductListing);
+    }
+
+    Render() {
+        return {
+            template: SideLoad('./components/templates/compact-listing.html'),
+            style: SideLoad('./components/css/compact-listing.css'),
+        };
+    }
+
+    OnceRendered() {
+
+    }
+}
+
+RegisterComponent(CompactProductListing);
