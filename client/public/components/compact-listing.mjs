07ae52839c13715cbb196113d9801514eaca8b13
Benjamin Kyd <benjaminkyd@gmail.com>
2022-03-25 21:54:46 +0000

    :art:
    Former-commit-id: 4b21252b2dc68423e4150b9ffb60e607e81650ce

--- client/public/components/compact-listing.mjs
+++ client/public/components/compact-listing.mjs
@@ -10,14 +10,18 @@ class CompactProductListing extends Component {
     Render() {
         return {
             template: `
-                {this.state.name}
-                {this.state.desc}
-                £{this.state.price}
                 <img src="{this.state.image}"></img>
+                {this.state.name} <p>
+                {this.state.desc} <p>
+                £{this.state.price}
             `,
             style: `
-                compact-listing-component {    
-                    display: flex;        
+                compact-listing-component {
+                    display: flex;
+                    text-align: center;
+                    width: 200px;
+                    flex-direction: column;
+                    margin: 10px;
                 }
             `,
         };
