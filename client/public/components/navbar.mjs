09611cd9784e6cec269a221eb939f7fc2338969c
Benjamin Kyd <benjaminkyd@gmail.com>
2022-04-07 16:15:54 +0100

    auth
    Former-commit-id: fbaf3eb1cb184e2e6e7ba8ed1b641f5bfb1ee18a

--- client/public/components/navbar.mjs
+++ client/public/components/navbar.mjs
@@ -1,10 +1,25 @@
 import { RegisterComponent, Component, SideLoad } from './components.mjs';
+import { LoginSignup, Signout } from '../auth.mjs';
+
+// due to peculiarities in asynchronus loading of components,
+// we need to have this remember the state of the logged in user
+// so that we can display the correct navbar
+let navbarCallback = null;
+export function NotifyNavbar(type, user) {
+    if (navbarCallback && type === 'login') {
+        navbarCallback.OnLogin(user);
+    }
+    if (navbarCallback && type === 'logout') {
+        navbarCallback.OnLogout();
+    }
+}
 
 class NavBar extends Component {
     static __IDENTIFY() { return 'navbar'; }
 
     constructor() {
         super(NavBar);
+        navbarCallback = this;
     }
 
     Render() {
@@ -14,7 +29,7 @@ class NavBar extends Component {
         };
     }
 
-    OnRender() {
+    SetupHamburger() {
         const menuToggler = document.querySelector('navbar-component').shadowRoot.querySelector('#menu-toggler');
         const navMenu = document.querySelector('navbar-component').shadowRoot.querySelector('.navbar');
 
@@ -22,9 +37,42 @@ class NavBar extends Component {
             menuToggler.classList.toggle('menu-active');
             navMenu.classList.toggle('menu-active');
         });
+    }
 
-        // set up basket
-        const basketToggler = document.querySelector('#basket-count');
+    OnLogin(user) {
+        const account = this.root.querySelector('.account-item');
+
+        // doing this with proper dom manipulation wasn't working
+        account.innerHTML = `
+            <a class="nav-link" href="#">${user.name}▾</a>
+            <ul class="sub-nav" >
+                <li><a class="sub-nav-link" href="#">My Account</a></li>
+                <li><a class="sub-nav-link logout-button" href="#">Log Out</a></li>
+            </ul>
+        `;
+
+        const logoutButton = account.querySelector('.logout-button');
+        logoutButton.addEventListener('click', () => {
+            Signout();
+        });
+    }
+
+    OnLogout() {
+        const account = this.root.querySelector('.account-item');
+        account.innerHTML = '<a class="account-button nav-link" href="#">My Account</a>';
+    }
+
+    OnRender() {
+        this.SetupHamburger();
+
+        // setup log in button
+        const loginButton = this.root.querySelector('.account-button');
+        loginButton.addEventListener('click', () => {
+            LoginSignup(this);
+            // remove event listener and then change the
+            // text to the users name and a dropdown that gives
+            // them the option to log out
+        });
     }
 }
 
--- client/public/components/storefront.mjs
+++ client/public/components/storefront.mjs
@@ -10,6 +10,25 @@ class StoreFront extends Component {
     Render() {
         return {
             template: `
+                <div class="main-carousel">
+                    <div class="carousel-cell">
+                        <img class="carousel-image" src="/res/lego-image1.jpg" alt="">
+                        <div class="carousel-caption">
+                            <h1>Welcome to LegoLog!</h1>
+                            <button>Shop LEGO® Now</button>
+                        </div>
+                    </div>
+                    <div class="carousel-cell">
+                        <img src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="">
+                    </div>
+                    <div class="carousel-cell">
+                        <img src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="">
+                    </div>
+                    <div class="carousel-cell">
+                        <img src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="">
+                    </div>
+                </div>
+
                 <product-list-component id="featured"
                                         title="Featured Lego Sets"
                                         getroute="/api/sets/featured"
@@ -17,6 +36,64 @@ class StoreFront extends Component {
                 </product-list-component>
                 `,
             style: `
+                @import url('https://unpkg.com/flickity@2/dist/flickity.min.css');
+                
+                /* enable Flickity by default */
+                .main-carousel:after {
+                    content: 'flickity';
+                    display: none; /* hide :after */
+                }
+
+                .carousel-image {
+                    object-fit: none;
+                    /* center image */
+                    display: block;
+                    margin: 0 auto;
+                    width: 100%;
+                    height: 100%;
+                }
+
+                .carousel-cell {
+                    height: 533px;
+                    width: 800px;
+                    margin-right : 10px;
+                }
+
+                .carousel-caption {
+                    position: absolute;
+                    right: 0;
+                    left: 0;
+                    bottom: 0;
+                    text-align: center;
+                    padding: 10px;
+                }
+
+                .carousel-caption h1 {
+                    font-size: 2em;
+                    color: #fff;
+                    text-shadow: 0 0 5px #000;
+                    font-weight: bold;
+                }
+
+                .carousel-caption p {
+                }
+
+                .carousel-caption button {
+                    background-color: #E55744;
+                    color: #fff;
+                    text-shadow: 0 0 3px #000;
+                    border: none;
+                    padding: 10px;
+                    font-size: 2em;
+                    font-weight: bold;
+                    border-radius: 5px;
+                }
+
+                .carousel-caption button:hover {
+                    background-color: #D7331D;
+                    cursor: pointer;
+                }
+
                 product-list-component {
                     z-index: 0;
                     display: block;
@@ -26,7 +103,17 @@ class StoreFront extends Component {
     }
 
     OnRender() {
-
+        // setup flickity
+        const carousel = this.root.querySelector('.main-carousel');
+        this.flkty = new window.Flickity(carousel, {
+            cellAlign: 'center',
+            contain: true,
+            wrapAround: true,
+            watchCSS: true,
+            autoPlay: true,
+            prevNextButtons: true,
+            pageDots: true,
+        });
     }
 }
 
