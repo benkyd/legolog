22112a519b370e5b11425af18cec88cbe7fe3267
Benjamin Kyd <benjaminkyd@gmail.com>
2022-04-08 02:00:21 +0100

    authentication
    Former-commit-id: 68b7736291339d2eb84936741d5dfb5c1bf8f4f7

--- src/routes/auth0-router.js
+++ src/routes/auth0-router.js
@@ -1,8 +1,14 @@
+// Loosely based on https://github.com/portsoc/auth0-example/blob/main/stages/6/server/auth0-helpers.js
+
+const Logger = require('../logger.js');
+
+const Axios = require('axios');
 const OAuth2JWTBearer = require('express-oauth2-jwt-bearer');
 
 const AUTH0CONFIG = {
-    audience: 'localhost:8080/api',
     domain: 'benkyd.eu.auth0.com',
+    clientId: 'WAOkscCNYD4FzXrm6pEQi3oNKNfa8l1F',
+    audience: 'localhost:8080/api',
 };
 
 const JWTChecker = OAuth2JWTBearer.auth({
@@ -25,15 +31,37 @@ function JWTMiddleware(req, res, next) {
     });
 }
 
-function LoginCheck(req, res, next) {
-    if (req.user) {
-        next();
-    } else {
-        res.status(401).send('Unauthorized');
+async function Auth0GetUser(req) {
+    if (!req.auth) {
+        return null;
     }
+
+    if (!req.auth || !req.auth.token) return null;
+
+    try {
+        const response = await Axios.get(`https://${AUTH0CONFIG.domain}/userinfo`, {
+            method: 'GET',
+            headers: {
+                authorization: `Bearer ${req.auth.token}`,
+            },
+        });
+
+        return response.data;
+    } catch (err) {
+        Logger.Error('error getting auth profile', req.auth, err);
+        return null;
+    }
+}
+
+async function Login(req, res) {
+    // tell the user all is well
+    res.send('Authenticated user: ' + req.auth.payload.sub);
+
+    // tell the database the user is new if they don't already exist
+    const user = await Auth0GetUser(req);
 }
 
 module.exports = {
     JWTMiddleware,
-    LoginCheck,
+    Login,
 };
